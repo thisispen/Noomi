bcbf16ce60590ccd9cf92dbc72c362ca70c78851
thisispen <karloflo0613@>
2025-03-30 21:23:42 -0500

    Initial Noomi MVP commit

---
+++ NoomiTests/NoomiTests.swift
@@ -0,0 +1,16 @@
+//
+//  NoomiTests.swift
+//  NoomiTests
+//
+//  Created by Flores, Karlo on 3/30/25.
+//
+
+import Testing
+
+struct NoomiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
